81e4858142d452f2948535d6c48f723d741874d8
Md Arfan Khan <khan188993@gmail.com>
2022-02-07 23:11:08 +0600

    GTP-3 react Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+    Navbar,
+    Header,
+    Brand,
+    Gtp3,
+    Features,
+    Possibility,
+    CTA,
+    Blogs,
+    Footer,
+} from "./component-imports";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div class="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <Gtp3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blogs />
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/component-imports.js
@@ -0,0 +1,21 @@
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Brand from "./components/brand/Brand";
+import Gtp3 from "./components/gtp-3/Gtp3";
+import Features from "./components/features/Features";
+import Possibility from "./components/possibility/Possibility";
+import CTA from "./components/cta/CTA";
+import Blogs from "./components/blogs/Blogs";
+import Footer from "./components/footer/Footer";
+
+export {
+    Navbar,
+    Header,
+    Brand,
+    Gtp3,
+    Features,
+    Possibility,
+    CTA,
+    Blogs,
+    Footer,
+};
